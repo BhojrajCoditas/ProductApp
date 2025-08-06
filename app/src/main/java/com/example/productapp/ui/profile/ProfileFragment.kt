7cc4124047d88ef2c5951e1d16bff3488afb47b4
BhjrajCoditas <bhojraj.narware@coditas.com>
2025-08-06 11:51:14 +0530

    completed the navigation to profile screen and working on modifying the ui of pofile screen

--- app/src/main/java/com/example/productapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/productapp/ui/profile/ProfileFragment.kt
@@ -1,4 +1,34 @@
 package com.example.productapp.ui.profile
 
-class ProfileFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.productapp.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Static Data already set in XML, optionally you can set here
+        // Example: binding.editTextName.setText("Sophia Patel")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
